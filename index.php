8ea62dbed1c16d576cff7ad588b99ad44ac80822
Kashyap Bhatt <kashyap.bhatt15@gmail.com>
2013-10-29 21:05:37 -0400

    db connection

--- index.php
+++ index.php
@@ -9,6 +9,37 @@
 <h1>
 	FAX Status 
 </h1>
+<?php
+	
+	$url=parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+    $server = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $db = substr($url["path"],1);
+
+    $con = mysqli_connect($server, $username, $password, $db);
+            
+    
+    // mysql_select_db($db);
+    if (mysqli_connect_errno($con)) {
+		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	}
+	else {
+		echo "Connected";
+	}
+	echo "<hr>";
+	//$result = mysqli_query($con,"SELECT * FROM users");
+
+	/*while($row = mysqli_fetch_array($result))
+	{
+		echo $row['id'] . " " . $row['username'];
+		echo "<br>";
+	}*/
+
+	mysqli_close($con);
+
+?>
 </body>
 </html>
 <?php
