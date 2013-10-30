5e998363bb0d625a85bd5540a0316ec35530cfb6
Kashyap Bhatt <kashyap.bhatt15@gmail.com>
2013-10-29 22:38:04 -0400

    db connection and store incoming sms in DB

--- sms-reply.php
+++ sms-reply.php
@@ -6,19 +6,13 @@
     if (!$con) {
         die('Could not connect: ' . mysql_error());
     }
-    echo 'Connected successfully';
     mysql_select_db($db);
 
-    /*$_POST["Body"] = "message body";
-    $_POST["From"] = "1212123434";
-    $_POST["SmsSid"] = "231212121344355654654654";*/
     $query = sprintf("INSERT INTO incoming (message, from_number, message_sid) VALUES ('%s','%s','%s')",
     mysql_real_escape_string($_POST["Body"]),
     mysql_real_escape_string($_POST["From"]),
     mysql_real_escape_string($_POST["SmsSid"]));
 
-//    echo $query;
-//    echo "<hr>";
     $result = mysql_query($query);
 
 
@@ -29,16 +23,11 @@
 
     mysql_close($con);
  
-    //echo 'Hello ' . htmlspecialchars($_POST["Body"]) . '!';
 
-    // now reply the sender
-    
+    // now reply the sender   
     header("content-type: text/xml");
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
 <Response>
-    <Message>Hi,thanks for the message! FAX Status will be up soon. Your message was <?php echo $_POST["Body"] ?></Message>
+    <Message>Hi,thanks for the message! FAX Status will be up soon. Your message was received and recorded. Contact 484-632-5324 for more information</Message>
 </Response>
-<?php
-//    echo "end";
-?>
