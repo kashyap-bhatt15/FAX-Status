28d5ab93830cadc296c3ec3dc2225c18afa2ca67
Kashyap Bhatt <kashyap.bhatt15@gmail.com>
2013-11-03 01:37:09 -0500

    sms test with same

--- sms-reply.php
+++ sms-reply.php
@@ -1,6 +1,7 @@
 <?php
     
     include "configure.php";
+    include "functions.php"
 
     $con = mysql_connect($server, $username, $password);
     if (!$con) {
@@ -22,7 +23,9 @@
     }
 
     /* Code  for removing spaces and getting Stop code */
-    
+    $stop_id = get_stop_id_from_stop_code($_POST["Body"]);
+
+
 
     mysql_close($con);
  
@@ -32,5 +35,53 @@
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
 <Response>
-    <Message>Hi,thanks for the message! FAX Status will be up soon. Your message was received and recorded. Contact 484-632-5324 for more information</Message>
+    <Message><?php echo $stop_id; ?> Hi,thanks for the message! FAX Status will be up soon. Your message was received and recorded. Contact 484-632-5324 for more information</Message>
 </Response>
+
+
+<?php
+/*
+<?php
+    
+    include "configure.php";
+    include "functions.php"
+
+    $con = mysql_connect($server, $username, $password);
+    if (!$con) {
+        die('Could not connect: ' . mysql_error());
+    }
+    mysql_select_db($db);
+
+    $query = sprintf("INSERT INTO incomings (message, from_number, message_sid, type) VALUES ('%s','%s','%s', 'text')",
+    mysql_real_escape_string($_POST["Body"]),
+    mysql_real_escape_string($_POST["From"]),
+    mysql_real_escape_string($_POST["SmsSid"]));
+
+    $result = mysql_query($query);
+
+
+    if (!$result) {
+        $message  = 'Invalid query: ' . mysql_error() . "\n";
+        $message .= 'Whole query: ' . $query;
+    }
+
+  
+    $stop_code = preg_replace('/\s+/', '', $_POST["Body"]);
+    $stop_id = get_stop_id_from_stop_code($stop_code);
+    if ($stop_id == -1) {
+        $message = "System is not working because of technical reasons. Please check back later.";
+    }
+    elseif($stop_id = 0) {
+        $message = "Please enter correct bus stop to get information. Check your bus stop where you at.";
+    }
+    else {
+        $message = "You are at " . $stop_id . ". Next " . "bus (bus number)" . "will arrive shortly";
+    }
+
+    mysql_close($con);
+ 
+
+    // now reply the sender   
+    header("content-type: text/xml");
+    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+?>
